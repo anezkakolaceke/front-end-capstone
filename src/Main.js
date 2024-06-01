85ca798d03d1bbd76b70c754ff3b9748231235d4
Anezka <nezka.slogowickt@gmail.com>
2024-06-01 17:22:01 +0100

    changes to styling of main page

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,11 @@
 import photo1 from './Mario and Adrian A.jpg'
 import photo2 from './Mario and Adrian b.jpg'
 import greeksalad from './greek salad.jpg'
+import bruschetta from './bruchetta.svg'
+import lemon from './lemon dessert.jpg'
+import dish from './Dish icon.svg'
+import photo from './photo.png'
+import star from './star.png'
 
 function Main(){
     return (
@@ -25,18 +30,18 @@ function Main(){
                     </article>
 
                     <article class="item">
-                        <img class="item_image" src={greeksalad} />
-                        <h1 class="item_header">Greek Salad</h1>
-                        <p class="item_text">The famous greek salad of crispy lettuce, peppers, 
-                        olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                        <img class="item_image" src={bruschetta} />
+                        <h1 class="item_header">Bruschetta</h1>
+                        <p class="item_text">Our Bruschetta is made from grilled bread that has been 
+                        smeared with garlic and seasoned with salt and olive oil. </p>
                         <p class="item_text"><a href="delivery.html">Order a delivery!</a></p>
                     </article>
 
                     <article class="item">
-                        <img class="item_image" src={greeksalad} />
-                        <h1 class="item_header">Greek Salad</h1>
-                        <p class="item_text">The famous greek salad of crispy lettuce, peppers, 
-                        olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                        <img class="item_image" src={lemon} />
+                        <h1 class="item_header">Lemon Dessert</h1>
+                        <p class="item_text">This comes straight from grandma’s recipe book, every last 
+                        ingredient has been sourced and is as authentic as can be imagined.</p>
                         <p class="item_text"><a href="delivery.html">Order a delivery!</a></p>
                     </article>
                 </div>
@@ -46,6 +51,49 @@ function Main(){
             <div class="testimonials">
                 <div class="main_title">
                     Testimonials
+                </div>
+
+                <div class="testimonial_items">
+                    <article class="test">
+                        <p class="test_rating">Rating: </p>
+                        <img src={star} alt="rating" class="star_rating1"/>
+                        <img src={star} alt="rating" class="star_rating2"/>
+                        <img src={star} alt="rating" class="star_rating3"/>
+                        <img src={star} alt="rating" class="star_rating4"/>
+                        <img src={star} alt="rating" class="star_rating5"/>
+                        <img class="test_photo" src={photo} />
+                        <p class="test_name">John Smith</p>
+                        <p class="test_comment">The restaurant was beautiful, staff is friendly and food was delicious!</p>
+                    </article>
+                    <article class="test">
+                        <p class="test_rating">Rating: </p>
+                        <img src={star} alt="rating" class="star_rating1"/>
+                        <img src={star} alt="rating" class="star_rating2"/>
+                        <img src={star} alt="rating" class="star_rating3"/>
+                        <img src={star} alt="rating" class="star_rating4"/>
+                        <img class="test_photo" src={photo} />
+                        <p class="test_name">John Smith</p>
+                        <p class="test_comment">The restaurant was beautiful, staff is friendly and food was delicious!</p>
+                    </article>
+                    <article class="test">
+                        <p class="test_rating">Rating: </p>
+                        <img src={star} alt="rating" class="star_rating1"/>
+                        <img src={star} alt="rating" class="star_rating2"/>
+                        <img src={star} alt="rating" class="star_rating3"/>
+                        <img class="test_photo" src={photo} />
+                        <p class="test_name">John Smith</p>
+                        <p class="test_comment">The restaurant was beautiful, staff is friendly and food was delicious!</p>
+                    </article>
+                    <article class="test">
+                        <p class="test_rating">Rating: </p>
+                        <img src={star} alt="rating" class="star_rating1"/>
+                        <img src={star} alt="rating" class="star_rating2"/>
+                        <img src={star} alt="rating" class="star_rating3"/>
+                        <img src={star} alt="rating" class="star_rating4"/>
+                        <img class="test_photo" src={photo} />
+                        <p class="test_name">John Smith</p>
+                        <p class="test_comment">The restaurant was beautiful, staff is friendly and food was delicious!</p>
+                    </article>  
                 </div>
             </div>
 
